7f4ab38143fe99069a92e7030c095efb45ce6eb9
ZHANGWENXIN <zwx72star@163.com>
2022-11-09 13:51:06 +0800

    python def

--- src/mypackage/learn.py
+++ src/mypackage/learn.py
@@ -1,5 +1,7 @@
 def star(name: str) -> str:
     """Return the description of input name.
+
     :param name: the input name.
-    :return the output description."""
+    :return the output description.
+    """
     return 'name: {0}'.format(name)
