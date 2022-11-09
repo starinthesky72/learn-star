564ab4791732f1a0392afceca3b143bc713ff6e1
ZHANGWENXIN <zwx72star@163.com>
2022-11-09 13:44:23 +0800

    python def

--- src/mypackage/learn.py
+++ src/mypackage/learn.py
@@ -1,2 +1,5 @@
-def star():
-    print("Study Hard!!!")
+def star(name: str) -> str:
+    """Return the description of input name.
+    :param name: the input name.
+    :return the output description."""
+    return 'name: {0}'.format(name)
