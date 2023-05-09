f064e7d35572506d632e544e7211d0ec5a18d7fb
starinthesky72 <zwx72star@163.com>
2023-05-09 10:33:49 +0800

    修改learn.py

--- src/mypackage/learn.py
+++ src/mypackage/learn.py
@@ -1,3 +1,6 @@
+import os
+
+
 def star(name: str) -> str:
     """Return the description of input name.
 
@@ -6,3 +9,20 @@ def star(name: str) -> str:
     :returns: the output description.
     """
     return 'name: {0}'.format(name)
+
+
+path = r'D:\metal\usefudata\1\9'
+total_txt = os.listdir(path)
+
+for file_name in total_txt:
+    file_path = os.path.join(path, file_name)
+
+    with open(file_path, 'r+') as file1:
+        lines = file1.readlines()
+        for index, line in enumerate(lines):
+            if index == 2:
+                lines[index] = '%nprocshared=16\n'
+
+        file1.seek(0)  # 将文件指针移到文件开头
+        file1.writelines(lines)
+        file1.truncate()  # 清空文件多余内容
